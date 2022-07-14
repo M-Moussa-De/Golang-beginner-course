99f9a8dce0213a00d39a2ccb4bacd758659c9238
Mahmoud Moussa <m.moussa@ratio-software.de>
2022-07-14 11:24:43 +0200

    functions

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func greetings(name string) string {
+	return "Hello " + name
+}
+
+func getSum(num1, num2, num3 int) int {
+	return num1 + num2 + num3
+}
+
 func main() {
-	fmt.Println("Hello Go")
+
+	name := "Golang"
+	fmt.Println(greetings(name))
+
+	sum := getSum(10, 15, 8)
+	fmt.Println(sum)
 }
