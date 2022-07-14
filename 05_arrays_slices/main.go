f8ee708ed41d4d73c9d0d2882af7465016eb9709
Mahmoud Moussa <m.moussa@ratio-software.de>
2022-07-14 11:16:05 +0200

    array vs slices

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Go")
+	// Arrays => must have a fest length and can't be expanded once it's declared
+	// arr :=  [3]string{"Banana", "Orange", "Pineapple"}
+
+	// Slice => In opposite of arrays, its size is dynamic
+	arraySlice := []string{"Banana", "Orange", "Pineapple"}
+
+	fmt.Println(arraySlice[0:3]) // Range prints the elements at index from 0 to 2 but 3 not included
+
+	fmt.Println(len(arraySlice))  // Length
+
+	arraySlice = append(arraySlice, "Mango") // Append one new element
+	arraySlice = append(arraySlice, "Peach", "Apple", "Stawberry") // Append several elements
+
+	arraySlice[0] = "Kiwi" // Replace element with another
+
+	fmt.Println(arraySlice)
 }
